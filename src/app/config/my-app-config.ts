ddbd5eb3169226b3575078e44b3ced17aed09651
Egemen <avniegemen1243@gmail.com>
2023-07-04 21:26:04 +0300

    Converting to desktop application with electron

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,8 +1,8 @@
 export default {
 
     oidc: {
-        clientId: '0oa9x42fib27TfkXs5d7',
-        issuer: 'https://dev-91432587.okta.com/oauth2/default',
+        clientId: '0oa9x4nl2vWr52Yq65d7',
+        issuer: 'https://dev-19542043.okta.com/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
