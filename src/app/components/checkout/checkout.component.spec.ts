1a7c0b4fc02687a31819055805f07b37158345ae
Egemen <avniegemen1243@gmail.com>
2023-07-26 20:11:18 +0300

    checkout test

--- src/app/components/checkout/checkout.component.spec.ts
+++ src/app/components/checkout/checkout.component.spec.ts
@@ -1,28 +1,31 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CheckoutComponent } from './checkout.component';
-import { CartService } from 'src/app/services/cart.service';
-import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
-import { CheckoutService } from 'src/app/services/checkout.service';
-import { Router } from '@angular/router';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CartService } from '../../services/cart.service';
+import { CheckoutService } from '../../services/checkout.service';
+import { Luv2ShopFormService } from '../../services/luv2-shop-form.service';
+import { CheckoutComponent } from './checkout.component';
+import { Country } from '../../common/country';
+import { of } from 'rxjs';
+import { Address } from '../../common/address';
 
 fdescribe('CheckoutComponent', () => {
   let component: CheckoutComponent;
   let fixture: ComponentFixture<CheckoutComponent>;
-
+  let formSerivce: Luv2ShopFormService;
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
+      await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule,ReactiveFormsModule],
       declarations: [CheckoutComponent],
-      providers: [CartService,Luv2ShopFormService,CheckoutService],
+      providers: [CartService,CheckoutService,Luv2ShopFormService],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CheckoutComponent);
     component = fixture.componentInstance;
+    formSerivce = TestBed.inject(Luv2ShopFormService);
     fixture.detectChanges();
   });
 
@@ -30,23 +33,16 @@ fdescribe('CheckoutComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should initialize form controls', () => {
-    expect(component.checkoutFormGroup).toBeDefined();
-    expect(component.firstName).toBeDefined();
-    expect(component.lastName).toBeDefined();
-    expect(component.email).toBeDefined();
-  });
 
-  it('should validate form controls', () => {
-    component.firstName?.setValue('');
-    component.lastName?.setValue('');
-    component.email?.setValue('invalid-email');
+  it('should get countries',()=>{
+    const countries:Country[] =[{id:1,name:'USA',code:'USA'}]
+    const months: number[] = [5,6,7,8,9,10,11,12];
+    spyOn(formSerivce,'getCreditCardMonths').and.returnValue(of(months))
+    spyOn(formSerivce,'getCountries').and.returnValue(of(countries))
 
-    fixture.detectChanges();
-
-    expect(component.firstName?.invalid).toBeTruthy();
-    expect(component.lastName?.invalid).toBeTruthy();
-    expect(component.email?.invalid).toBeTruthy();
+    component.ngOnInit();
+    expect(component.creditCardMonths).toBe(months);
+    expect(component.countries).toBe(countries)
   });
 
   it('should call onSubmit when form is valid', () => {
@@ -58,6 +54,18 @@ fdescribe('CheckoutComponent', () => {
     component.onSubmit();
 
     expect(component.onSubmit).toHaveBeenCalled();
+  });
+
+  it('should shipping adderss and billing address equal',()=>{
+    
+    component.copyShippingAddressToBillingAddress(true);
+    expect(component.billingAddressStates).toBe(component.shippingAddressStates);
+  });
+
+  it('should shipping adderss and billing address not equal',()=>{
+    component.copyShippingAddressToBillingAddress(true);
+    expect(component.billingAddressStates).not.toBe(component.shippingAddressStates);
+
   });
 
 });
